2d42f727c863d672e5cf2af9fcb170825489b41d
adensur <gayduk.m@natalie-tours.ru>
2014-09-22 12:34:11 +0400

    Uploading plot example with nice R graphics usin base plotting system

---
+++ plotexample.R
@@ -0,0 +1,21 @@
+library(plyr)
+# read in the data; the SCC data is not even required for this plot
+# assumption - the data files are in the same directory as this file
+NEI <- readRDS("./summarySCC_PM25.rds")
+# open a device for outputting the plot
+png("./plot1.png", height=480,width=480)
+# get the summarised emissions data for all of the US, by year
+neiAll <- ddply(NEI,.(year),summarise,sumOf=sum(Emissions))
+# make a (simple) plot
+plot(neiAll$year, neiAll$sumOf/1000, xlab="Year",
+     ylab="PM2.5 Emissions (kilotons)",
+     main="PM2.5 Emissions for USA, 1999-2008",
+     xaxt="n",type="b",lwd=2,
+     col="red", cex.main=0.9,xlim=c(1999,2009))
+# make the X axis by "year"
+axis(1,at=neiAll$year)
+# write the summarised Emission values on the plot
+text(neiAll$year,neiAll$sumOf/1000,as.integer(neiAll$sumOf/1000),
+     pos=4,col="blue",cex=0.8)
+# close the device
+dev.off()
